f25accac10ecfca680cec4c351e33973eb798634
Tim Crall <timcrall@gmail.com>
2019-03-08 14:13:04 -0800

    wtf

--- main.tf
+++ main.tf
@@ -109,7 +109,7 @@ resource "null_resource" "dockeree_run_init"
     password      = "${var.ssh_password}"
     private_key   = "${var.private_key}"
     bastion_host  = "${var.bastion_host}"
-    script_path   = "${var.script_path}/terraform"
+    script_path   = "${var.script_path}/terraform_exec"
   }
   provisioner "remote-exec" {
     inline = [
