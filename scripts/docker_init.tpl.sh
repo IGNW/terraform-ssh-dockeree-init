76509b7abd23ea189e204753ffaa749b74245f41
Tim Crall <timcrall@gmail.com>
2019-04-02 15:43:54 -0700

    Don't https twice

--- scripts/docker_init.tpl.sh
+++ scripts/docker_init.tpl.sh
@@ -227,7 +227,7 @@ function configure_s3_dtr_storage {
   debug "S3 bucket: ${dtr_s3_bucket}"
   HTTP_CODE=$(curl -k --write-out '%%{http_code}' \
    -u "${ucp_admin_username}":"${ucp_admin_password}" \
-   -X PUT "https://${ucp_url}/api/v0/admin/settings/registry/simple" \
+   -X PUT "${ucp_url}/api/v0/admin/settings/registry/simple" \
    -H 'content-type: application/json' \
    -d "{\"storage\":{\"delete\":{\"enabled\":true},\"maintenance\":{\"readonly\":{\"enabled\":false}},\"s3\":{\"rootdirectory\":\"\",\"region\":\"${dtr_s3_region}\",\"regionendpoint\":\"\",\"bucket\":\"${dtr_s3_bucket}\",\"secure\": true}}}")
    debug "HTTP_CODE: $HTTP_CODE"
